762191ac56ae17f776765cd3318a38a13b0649ce
mobydoby <andyjma.work@gmail.com>
2022-09-01 19:30:10 -0400

    first commit

---
+++ hello.c
@@ -0,0 +1,128 @@
+//------------------------------------------------------------------------------------
+// Hello.c
+//------------------------------------------------------------------------------------
+//
+// Test program to demonstrate serial port I/O.  This program writes a message on
+// the console using the printf() function, and reads characters using the getchar()
+// function.  An ANSI escape sequence is used to clear the screen if a '2' is typed.
+// A '1' repeats the message and the program responds to other input characters with
+// an appropriate message.
+//
+// Any valid keystroke turns on the green LED on the board; invalid entries turn it off
+//
+
+
+//------------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------------
+#include "stm32f769xx.h"
+#include "hello.h"
+
+#include<stdint.h>
+
+char choice;
+//------------------------------------------------------------------------------------
+// MAIN Routine
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    Sys_Init(); // This always goes at the top of main (defined in init.c)
+
+	char in[3];
+    int size = 3;
+
+    printf("\033[2J\033[;H"); // Erase screen & move cursor to home position
+    fflush(stdout); // Need to flush stdout after using printf that doesn't end in \n
+//    printf("Test of the printf() function.\n\n");
+
+
+    // Need to enable clock for peripheral bus on GPIO Port J
+    __HAL_RCC_GPIOJ_CLK_ENABLE(); 	// Through HAL
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOJEN; // or through registers
+    //// Below two lines are example on how to access a register by knowing it's memory address
+    //volatile uint32_t * RCC_AHB1_CLOCK_ENABLE = (uint32_t*) 0x40023830U; // Enable clock for peripheral bus on GPIO Port J
+    //*RCC_AHB1_CLOCK_ENABLE |= 512U; // Bitmask for RCC AHB1 initialization: 0x00000200U or 512U in decimal
+
+    GPIOJ->MODER |= 1024U; //Bitmask for GPIO J Pin 5 initialization (set it to Output mode): 0x00000400U or 1024U in decimal
+    GPIOJ->BSRR = (uint16_t)GPIO_PIN_5; // Turn on Green LED (LED2)
+    GPIOJ->BSRR = (uint32_t)GPIO_PIN_5 << 16; // Turn off Green LED (LED2)
+    GPIOJ->ODR ^= (uint16_t)GPIO_PIN_5; // Toggle LED2
+
+// It doesn't get lower level than this!
+//    volatile uint32_t * GREENLEDMODER = (uint32_t*) 0x40022400U; // Init GPIO J Pin 5 (LED2 with no Alt. func.) to Output
+//    *GREENLEDMODER |= 1024U; // Bitmask for GPIO J Pin 5 initialization: 0x00000400U or 1024U in decimal
+
+    volatile uint32_t * GREENLEDBSRR = (uint32_t*) 0x40022418U; // Address of GPIO J Bit Set/Reset Register
+    *GREENLEDBSRR = (uint16_t)0x0020U; // Turn on Green LED (LED2)
+
+    HAL_Delay(1000); // Pause for a second
+
+//    volatile uint32_t * GREENLEDODR = (uint32_t*) 0x40022414U; // Address of GPIO J Output Data Register
+//    *GREENLEDODR ^= (uint16_t)0x0020U; // Toggle Green LED (LED2)
+
+    int ins = 0, break_num;
+    while(1){
+    	//to change entire terminal, print this inside loop ?? don't really understand why.
+    	printf("\033[0;33;5;44m");
+    	fflush(stdout);
+    	if (ins == 0){
+    		printf("\n                         PRESS <ESC> OR <CTL>+[ TO QUIT\n\n\n\r\n");
+    		ins = 1;
+    	}
+
+    	char choice;
+    	choice = getchar();
+
+//    	if the inputted string is an acceptable string - print - TASK1
+    	if (choice == 0x1b){
+//    		printf("escape\r\n");
+    		break;
+    	}
+    	else if (choice < 0x20){
+    		break_num = 6;
+    		while(break_num>0){
+    			printf("\n");
+    			break_num--;
+    		}
+    		printf("The key board character $XX is \033[4m'not printable'\a\r\n");
+    	}
+    	else{
+    		printf("The keyboard character is \033[31m%c", choice);
+    	}
+    }
+}
+
+//------------------------------------------------------------------------------------
+//Extra thing to consider...
+//------------------------------------------------------------------------------------
+void serial_print_things(void) {
+	//Input Buffer
+	char input[2];
+	input[0]=0;
+	input[1]=0;
+
+	//Initialize the system
+	Sys_Init();
+	initUart(&Second_UART, 9600, USART6); // Allow printing over USART6 (Arduino pins D0 - Rx and D1 - TX)
+	uart_print(&USB_UART, "\033[2J\033[;H");
+	uart_print(&Second_UART, "\033[2J\033[;H");
+	uart_print(&USB_UART, "Hello World: This is the USB Serial Connection\r\n");
+	uart_print(&Second_UART, "Hello World: This is the other UART Connection\r\n");
+	uart_print(&USB_UART, "In order to send newlines to this terminal,\r\n");
+	uart_print(&USB_UART, "Press <ESC> and type \"[20h\" (without quotes)\r\n");
+	uart_print(&USB_UART, "To get out of newline mode and back to line feed mode,\r\n");
+	uart_print(&USB_UART, "Press <ESC> and type \"[20l\" (without quotes)\r\n");
+	uart_print(&USB_UART, "\"Thanks for Playing!\"\r\n");
+
+	printf("THIS SENTENCE USES PRINTF!!!\r\n");
+  // Don't forget to end printf with newline or run fflush(stdout) after it!
+
+	while(1) {
+		input[0]=uart_getchar(&USB_UART, 0);
+		uart_print(&USB_UART, input);
+		uart_print(&Second_UART, input);
+	}
+
+	while(1);// HALT AND CATCH FIRE
+}
+
